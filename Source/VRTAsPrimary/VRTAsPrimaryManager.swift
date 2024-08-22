87743e8a4fb72b60cffd59808f9c5071b5ea6379
Scott McCoy <scott.mccoy@vrtcal.com>
2024-08-22 12:30:55 -0700

    Fix: Ads only load on first init

--- Source/VRTAsPrimary/VRTAsPrimaryManager.swift
+++ Source/VRTAsPrimary/VRTAsPrimaryManager.swift
@@ -13,6 +13,7 @@ class VRTAsPrimaryManager: NSObject {
     ) {
         VRTLogInfo()
         guard shouldInit else {
+            completionHandler(.success())
             return
         }
         
